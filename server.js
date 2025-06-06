71126785e7983f33c01ba4036d68de4ad2b7cb40
Invia-shubham <shubham.singh@invia.co.in>
2025-06-06 12:36:23 +0530

    add custom option in call

--- server.js
+++ server.js
@@ -1,26 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-io.on('connection', (socket) => {
-  console.log('User connected:', socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
 
-  socket.on('join-room', (roomId) => {
+  socket.on("join-room", (roomId) => {
     socket.join(roomId);
-    socket.to(roomId).emit('user-connected', socket.id);
+    socket.to(roomId).emit("user-connected", socket.id);
 
-    socket.on('signal', (data) => {
-      // Send signaling data to other users in the room
-      socket.to(roomId).emit('signal', { id: socket.id, signal: data });
+    socket.on("signal", (data) => {
+      socket.to(roomId).emit("signal", { id: socket.id, signal: data });
     });
 
-    socket.on('disconnect', () => {
-      socket.to(roomId).emit('user-disconnected', socket.id);
+    // Handle disconnect-call from client
+    socket.on("disconnect-call", (roomId) => {
+      socket.to(roomId).emit("user-disconnected", socket.id);
+    });
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", socket.id);
+    });
+    socket.on("end-call", (roomId) => {
+      io.in(roomId).emit("call-ended"); // send to everyone in the room
     });
   });
 });
