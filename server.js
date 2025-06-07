621eedcecfc1f52b269ccc3909699017af6485db
Invia-shubham <shubham.singh@invia.co.in>
2025-06-07 09:59:01 +0530

    fix full screen option and add chat option

--- server.js
+++ server.js
@@ -10,15 +10,22 @@ app.use(express.static("public"));
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // Handle joining a room
   socket.on("join-room", (roomId) => {
     socket.join(roomId);
     socket.to(roomId).emit("user-connected", socket.id);
 
+    // Handle incoming chat messages
+    socket.on("chat-message", (messageData) => {
+      const { message, roomId } = messageData;
+      io.to(roomId).emit("chat-message", { message: message, userId: socket.id });
+    });
+
+    // Handle other events
     socket.on("signal", (data) => {
       socket.to(roomId).emit("signal", { id: socket.id, signal: data });
     });
 
-    // Handle disconnect-call from client
     socket.on("disconnect-call", (roomId) => {
       socket.to(roomId).emit("user-disconnected", socket.id);
     });
@@ -26,11 +33,13 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
       socket.to(roomId).emit("user-disconnected", socket.id);
     });
+
     socket.on("end-call", (roomId) => {
       io.in(roomId).emit("call-ended"); // send to everyone in the room
     });
   });
 });
+
 
 http.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
